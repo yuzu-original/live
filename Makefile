a6bde37c9c85ef04e5ab2878ef5f4a08daa9d988
yuzu <yurkazubkov@yandex.ru>
2024-06-23 16:17:25 +0300

    sdl2 -> raylib

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
-PKGS=sdl2
-CFLAGS=-Wall -O3 -pedantic `pkg-config --cflags $(PKGS)`
-LIBS=`pkg-config --libs $(PKGS)` -lm
+CFLAGS=-Wall -O3 -pedantic -I./include/
+LIBS=-L./lib/ -lraylib -lm
 
 live: $(wildcard src/*.c) $(wildcard src/*.h)
 	$(CC) $(CFLAGS) -o live src/main.c $(LIBS)
--- src/main.c
+++ src/main.c
@@ -1,53 +1,20 @@
 #include "main.h"
-#include <SDL.h>
-#include <stdio.h>
+#include "raylib.h"
 
 
 int main(void) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Couldn't initialize SDL: %s\n", SDL_GetError());
-        return 1;
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Live");
+    
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Window", 0, 0, 20, LIGHTGRAY);
+        EndDrawing();
     }
 
-    SDL_Window *window = SDL_CreateWindow(
-        "Live",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
-        SDL_WINDOW_RESIZABLE);
-
-    if (!window) {
-        printf("Failed to open window: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(
-        window, -1,
-        SDL_RENDERER_ACCELERATED);
-
-    if (!renderer) {
-        printf("Failed to create renderer: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    int quit = 0;
-    while (!quit) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT: {
-                    quit = 1;
-                } break;
-            }
-        }
-        SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_Quit();
+    CloseWindow();
 
     return 0;
 }
