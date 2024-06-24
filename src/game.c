13e609d6c79bd03ea090debd9425ec12e5d958b5
yuzu <yurkazubkov@yandex.ru>
2024-06-24 18:33:47 +0300

    Added camera and grid

--- src/game.c
+++ src/game.c
@@ -1,16 +1,81 @@
 #include "game.h"
 #include "raylib.h"
+#include "raymath.h"
+#include <string.h>
 
+
+void DrawAgent(Vector2 pos, Agent *agent) {
+    DrawCircleV(
+        Vector2Scale(Vector2AddValue(pos, 0.5f), CELL_SIZE),
+        CELL_SIZE/2.5f,
+        RED);
+}
+
+void DrawGame(Game *game) {
+    for (int y = 0; y <= BOARD_HEIGHT; y++) {
+        DrawLine(0, y*CELL_SIZE, BOARD_WIDTH*CELL_SIZE, y*CELL_SIZE, GRAY);
+    }
+    for (int x = 0; x <= BOARD_WIDTH; x++) {
+        DrawLine(x*CELL_SIZE, 0, x*CELL_SIZE, BOARD_HEIGHT*CELL_SIZE, GRAY);
+    }
+    for (int y = 0; y < BOARD_HEIGHT; y++) {
+        for (int x = 0; x < BOARD_WIDTH; x++) {
+            if (game->agents[y][x].health > 0) {
+                DrawAgent((Vector2) {x, y}, &game->agents[y][x]);
+            }
+        }
+    }
+}
+
+void InitGame(Game *game) {
+    memset(game, 0, sizeof(*game));
+
+    game->agents[0][10].health = 100;
+    game->agents[1][10].health = 100;
+    game->agents[10][10].health = 100;
+    game->agents[0][0].health = 100;
+    game->agents[3][6].health = 100;
+}
 
 int main(void) {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Live");
-    
-    SetTargetFPS(60);
+    Game game = {0};
+    InitGame(&game);
 
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Live");
+
+    Camera2D camera = {0};
+    camera.zoom = 1.0f;
+
+    SetTargetFPS(60);
+    
     while (!WindowShouldClose()) {
+        // Update
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+            Vector2 delta = Vector2Scale(GetMouseDelta(), -1.0f/camera.zoom);
+            camera.target = Vector2Add(camera.target, delta);
+        }
+        
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0) {
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+            camera.offset = GetMousePosition();
+            camera.target = mouseWorldPos;
+
+            float scaleFactor = 1.0f + (0.25*fabsf(wheel));
+            if (wheel < 0) scaleFactor = 1.0f/scaleFactor;
+            camera.zoom = Clamp(camera.zoom*scaleFactor, 0.125f, 64.0f);
+        }
+
+        // Draw
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Window", 0, 0, 20, LIGHTGRAY);
+            ClearBackground(BLACK);
+            
+            BeginMode2D(camera);
+                DrawGame(&game);
+            EndMode2D();
+            
+            DrawFPS(0, 0);
         EndDrawing();
     }
 
--- src/game.h
+++ src/game.h
@@ -4,4 +4,21 @@
 #define SCREEN_WIDTH  1280
 #define SCREEN_HEIGHT 720
 
+#define BOARD_WIDTH  100
+#define BOARD_HEIGHT 100
+
+#define CELL_SIZE 32
+
+#define CHROMOSOME_SIZE = 10;
+
+typedef struct {
+    int health;
+} Agent;
+
+typedef struct {
+    Agent agents[BOARD_HEIGHT][BOARD_WIDTH];
+    int foods[BOARD_HEIGHT][BOARD_WIDTH];
+    int walls[BOARD_HEIGHT][BOARD_WIDTH];
+} Game;
+
 #endif
